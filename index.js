69d7e9e17123fe441cbcbc622feab731b579959f
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2024-01-09 19:20:45 +1100

    Added prompt questions to index.js

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const {Square, Triangle, Circle} = require('./lib/shapes');
 
+
+// promt questions 
+
+inquirer.prompt([
+
+    {
+      type: 'input',
+      name: 'typeText',
+      message: 'What would you like your logo to say? (MAX 3 chars)',
+    },
+
+    {
+        type: 'list',
+        name: 'typeShape',
+        message: 'What shape would you like your logo to be?',
+        choices: ['Square', 'Triangle', 'Circle'],
+      },
+
+    {
+      type: 'input',
+      name: 'colourText',
+      message: 'What colour would you like your text to be?',
+      default: '',
+    },
+
+    {
+      type: 'input',
+      name: 'colourShape',
+      message: 'What colour would you like your shape to be?',
+      default: '',
+    },
+
+  ])
